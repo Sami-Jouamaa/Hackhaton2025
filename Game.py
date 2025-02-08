f9fd158daed9745647ec247b2ad3cce428ea3cf9
Sami-Jouamaa <samijouamaa@gmail.com>
2025-02-08 15:54:55 -0500

    textures for each planet and the star

--- Game.py
+++ Game.py
@@ -8,7 +8,7 @@ pygame.init()
 screen = pygame.display.set_mode((1920, 1080))
 clock = pygame.time.Clock()
 renderer = PlanetRenderer(screen)
-solarSystem = SolarSystem((screen.get_width()/2, screen.get_height()/2), 8)
+solarSystem = SolarSystem((screen.get_width()/2, screen.get_height()/2))
 running = True
 selected_planet = None
 dt = 0
--- Planet.py
+++ Planet.py
@@ -5,21 +5,9 @@ import pygame.gfxdraw
 from pygame.locals import *
 
 # --- Couleurs ---
-# COLORS = {
-#     "life": "lifeTexture.jpg",
-#     "gas": "lifeTexture.jpg",
-#     "dead": "lifeTexture.jpg",
-#     "star": "lifeTexture.jpg",
-#     "background": (0, 0, 40)
-# }
 COLORS = {
-    "life": (34, 139, 34),
-    "gas": (255, 165, 0),
-    "dead": (139, 137, 137),
-    "star": (255, 255, 0),
     "background": (0, 0, 40)
 }
-
 
 # --- Classe de Base Planète ---
 class Planet:
@@ -41,7 +29,6 @@ class Planet:
                 "petrol": random.randint(200, 500),
                 "wood": random.randint(300, 700)
             }
-            self.color = COLORS["life"]
             self.radius = 20
             
         elif self.type == "gas":
@@ -50,7 +37,6 @@ class Planet:
                 "methane": random.randint(500, 1500),
                 "helium": random.randint(300, 800)
             }
-            self.color = COLORS["gas"]
             self.radius = 27
             
         elif self.type == "dead":
@@ -60,7 +46,6 @@ class Planet:
                 "gold": random.randint(100, 300),
                 "plutonium": random.randint(50, 150)
             }
-            self.color = COLORS["dead"]
             self.radius = 16
 
     def update_orbit(self, center, dt):
@@ -71,19 +56,74 @@ class Planet:
 
 # --- Système Solaire ---
 class SolarSystem:
-    def __init__(self, center, num_planets):
+    def __init__(self, center):
         self.center = center
         self.planets = []
         self.star_radius = 32
                 
-        # Génération procédurale de planètes
-        planet_types = ["life", "gas", "dead"]
-        for i in range(num_planets):
-            p_type = random.choice(planet_types)
-            orbit_r = 75 + i*50  # Distance orbitale
-            orbit_speed = 0.32 / (i+1)  # Plus lent avec la distance
-            planet = Planet(f"Planet {i+1}", p_type, orbit_r, orbit_speed)
-            self.planets.append(planet)
+        #Planet  6
+        p_type = "dead"
+        orbit_r = 75 + 0 * 50
+        orbit_speed = 0.32/(0 + 1)
+        planet = Planet(f"Planet {5+1}", p_type, orbit_r, orbit_speed)
+        planet.color = "planet06.png"
+        self.planets.append(planet)
+        
+        #Planet 3
+        p_type = "dead"
+        orbit_r = 75 + 1 * 50
+        orbit_speed = 0.32/(1 + 1)
+        planet = Planet(f"Planet {2+1}", p_type, orbit_r, orbit_speed)
+        planet.color = "planet03.png"
+        self.planets.append(planet)
+        
+        #Planet 4
+        p_type = "dead"
+        orbit_r = 75 + 2 * 50
+        orbit_speed = 0.32/(2 + 1)
+        planet = Planet(f"Planet {3+1}", p_type, orbit_r, orbit_speed)
+        planet.color = "planet04.png"
+        self.planets.append(planet)
+        
+        #Planet 2
+        p_type = "dead"
+        orbit_r = 75 + 3 * 50
+        orbit_speed = 0.32/(3 + 1)
+        planet = Planet(f"Planet {1+1}", p_type, orbit_r, orbit_speed)
+        planet.color = "planet02.png"
+        self.planets.append(planet)
+        
+        # Planet 1
+        p_type = "dead"
+        orbit_r = 75 + 4 * 50
+        orbit_speed = 0.32/(4 + 1)
+        planet = Planet(f"Planet {0+1}", p_type, orbit_r, orbit_speed)
+        planet.color = "planet01.png"
+        self.planets.append(planet)
+        
+        #Planet 8
+        p_type = "life"
+        orbit_r = 75 + 5 * 50
+        orbit_speed = 0.32/(5 + 1)
+        planet = Planet(f"Planet {7+1}", p_type, orbit_r, orbit_speed)
+        planet.color = "planet08.png"
+        self.planets.append(planet)
+        
+        #Planet 5
+        p_type = "life"
+        orbit_r = 75 + 6 * 50
+        orbit_speed = 0.32/(6 + 1)
+        planet = Planet(f"Planet {4+1}", p_type, orbit_r, orbit_speed)
+        planet.color = "planet05.png"
+        self.planets.append(planet)
+        
+        #Planet 7
+        p_type = "gas"
+        orbit_r = 75 + 7 * 50
+        orbit_speed = 0.32/(7 + 1)
+        planet = Planet(f"Planet {6+1}", p_type, orbit_r, orbit_speed)
+        planet.color = "planet07.png"
+        self.planets.append(planet)
 
     def update(self, dt):
         for planet in self.planets:
@@ -118,15 +158,15 @@ class PlanetRenderer:
 
     def draw(self, solar_system):
         # Dessiner l'étoile
-        pygame.gfxdraw.filled_circle(
-            self.screen,  
-            int(solar_system.center[0]),
-            int(solar_system.center[1]), 
-            int(solar_system.star_radius),
-            COLORS["star"]
-            )
-        # pygame.draw.circle(self.screen, COLORS["star"], 
-        #                   solar_system.center, solar_system.star_radius)
+        image = pygame.image.load("sunTexture.jpg")
+        star_surface = pygame.Surface((solar_system.star_radius * 2, solar_system.star_radius*2), pygame.SRCALPHA)
+        scaled_image = pygame.transform.scale(image, (solar_system.star_radius*2, solar_system.star_radius*2))
+        star_surface.blit(scaled_image, (0,0))
+        
+        mask = pygame.Surface((solar_system.star_radius*2, solar_system.star_radius*2), pygame.SRCALPHA)
+        pygame.draw.circle(mask, (255, 255, 255), (solar_system.star_radius, solar_system.star_radius), solar_system.star_radius)
+        star_surface.blit(mask, (0, 0), special_flags=pygame.BLEND_RGBA_MIN)
+        self.screen.blit(star_surface, (solar_system.center[0] - solar_system.star_radius, solar_system.center[1] - solar_system.star_radius))
         
         # Dessiner les orbites
         for planet in solar_system.planets:
@@ -137,9 +177,17 @@ class PlanetRenderer:
         
         # Dessiner les planètes
         for planet in solar_system.planets:
-            # pygame.gfxdraw.filled_circle(self.screen, planet.pos.x, planet.pos.y, planet.radius, texture, 0, 0)
-            pygame.draw.circle(self.screen, planet.color, planet.pos, planet.radius)
+            image = pygame.image.load(planet.color)
+            planet_surface = pygame.Surface((planet.radius * 2, planet.radius*2), pygame.SRCALPHA)
+            scaled_image = pygame.transform.scale(image, (planet.radius * 2, planet.radius*2))
+            planet_surface.blit(scaled_image, (0, 0))
             
+            mask = pygame.Surface((planet.radius * 2, planet.radius*2), pygame.SRCALPHA)
+            pygame.draw.circle(mask, (255, 255, 255), (planet.radius, planet.radius), planet.radius)
+            planet_surface.blit(mask, (0, 0), special_flags=pygame.BLEND_RGBA_MIN)
+            self.screen.blit(planet_surface, (planet.pos[0] - planet.radius, planet.pos[1] - planet.radius))
+            
+            # pygame.draw.circle(self.screen, planet.color, planet.pos, planet.radius)
             # Info-bulle
             text = self.font.render(f"{planet.name} ({planet.type})", True, (255, 255, 255))
             self.screen.blit(text, (planet.pos[0]+20, planet.pos[1]-10))
