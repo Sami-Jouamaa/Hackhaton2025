ad5928866cbdda35bccfbb6051f7236b4cdd04cd
hidriaziz <aziz.hidri@polymtl.ca>
2025-02-08 23:01:51 -0500

    feat : new planet UI buttons

--- ListPlanets.py
+++ ListPlanets.py
@@ -1,10 +1,132 @@
 planetList = {
-    "0": ["Infernis", 1800792, 12670, 0, "Morte", "Hell", "planet-bck/Infernis.pn"],
-    "1": ["Terre", 5972123923093, 16000, 8000000000, "Vie", "La planète bleue", "planet-bck/Earth.png"],
-    "2": ["Terralis Prime", 5920752, 20000, 1023, "Vie", "Reptilians", "planet-bck/Terralis_Prime.png"],
-    "3": ["Nekronia", 30000000, 13000, 0, "Morte", "Dead planet", "planet-bck/Nekronia.png"],
-    "4": ["Chloria", 20000000, 15000, 0, "Morte", "No one lives there", "planet-bck/Chloria.png"],
-    "5": ["Atlantis", 15000000, 220000, 0, "Vie", "Under the sea", "planet-bck/Atlantis.png"],
-    "6": ["Xenon Prime", 30000000000, 30000, 0, "Vie", "Purple guys", "planet-bck/Xenon_prime.png"],
-    "7": ["Cryogenia", 1230003, 8000, 0, "Vie", "Frozen land", "planet-bck/Cryogenia.png"],
+    "0": [
+        "Infernis",
+        1800792,
+        12670,
+        0,
+        "Morte",
+        "Enfer métallique. Tempêtes solaires mortelles.",
+        "planet-bck/Infernis.png",
+        "Noyau de fer en fusion sous une croûte de lave vitrifiée.",
+        {
+            "colonies": [],
+            "contrats": "Aucun contrat disponible.",
+            "inventaire": "Aucun inventaire disponible."
+        }
+    ],
+    "1": [
+        "Terre",
+        5972123923093,
+        16000,
+        8000000000,
+        "Vie",
+        "Planète-mère de l'humanité. Équilibre écologique strict.",
+        "planet-bck/Earth.png",
+        "Mégapoles sous dômes écologiques polués après des centaines d'années d'exploitation.",
+        {
+            "colonies": [],
+            "lois": "Loi environnementale: 10%\nTaxe sur l'extraction: 5%",
+            "marché": "Marché local actif.",
+            "contrats": "Contrat d'extraction A\nContrat de minage B",
+            "inventaire": "Inventaire: Ressources variées."
+        }
+    ],
+    "2": [
+        "Terralis Prime",
+        5920752,
+        20000,
+        1023,
+        "Vie",
+        "Symbiose parfaite entre Verdantari et écosystème.",
+        "planet-bck/Terralis_Prime.png",
+        "Cités-arbres géantes alimentées par photosynthèse quantique.",
+        {
+            "colonies": ["Proxima"],
+            "lois": "Loi environnementale: 10%\nTaxe minière: 5%",
+            "marché": "Marché actif: Produit X à 50 GLD, Produit Y à 35 GLD",
+            "contrats": "Contrat d'extraction A\nContrat de minage B",
+            "inventaire": "Matériaux: 100 tonnes\nÉquipement: 5 unités"
+        }
+    ],
+    "3": [
+        "Nekronia",
+        30000000,
+        13000,
+        0,
+        "Morte",
+        "Désert radioactif post-guerre.",
+        "planet-bck/Nekronia.png",
+        "Champs de ruines irradiées peuplés de drones dégénérés.",
+        {
+            "colonies": [],
+            "contrats": "Aucun contrat disponible.",
+            "inventaire": "Aucun inventaire disponible."
+        }
+    ],
+    "4": [
+        "Chloria",
+        20000000,
+        15000,
+        0,
+        "Morte",
+        "Monde toxique au chlore. Extraction risquée.",
+        "planet-bck/Chloria.png",
+        "Atmosphère corrosive de chlure d'hydrogène. Paysages de cristaux ionisés.",
+        {
+            "colonies": [],
+            "contrats": "Aucun contrat disponible.",
+            "inventaire": "Aucun inventaire disponible."
+        }
+    ],
+    "5": [
+        "Atlantis",
+        15000000,
+        220000,
+        0,
+        "Vie",
+        "Civilisation sous-marine. Tech bio-luminescente.",
+        "planet-bck/Atlantis.png",
+        "Cités de corail intelligents peuplées d'humanoïdes amphibies, intéressante pour la recherche biologique et pour récolter l'énergie géothermique.",
+        {
+            "colonies": [],
+            "lois": "Loi océanique: 8%\nTaxe de pêche: 3%",
+            "marché": "Marché maritime: Poisson à 20 GLD, Algues à 15 GLD",
+            "contrats": "Contrat de construction de barrages\nContrat de minage sous-marin",
+            "inventaire": "Produits de la mer: 200 tonnes\nÉquipement: 10 unités"
+        }
+    ],
+    "6": [
+        "Xenon Prime",
+        30000000000,
+        30000,
+        0,
+        "Vie",
+        "Géante de gaz nobles. Phénomènes énergétiques.",
+        "planet-bck/Xenon_prime.png",
+        "Nuages de xénon ionisé sous tempêtes électriques permanentes.",
+        {
+            "colonies": [],
+            "lois": "Loi des gaz: 12%\nTaxe sur la production énergétique: 6%",
+            "marché": "Marché du gaz: Gaz à 40 GLD",
+            "contrats": "Contrat de production énergétique\nContrat d'approvisionnement en gaz",
+            "inventaire": "Gaz: 500 unités\nÉquipements énergétiques: 20 unités"
+        }
+    ],
+    "7": [
+        "Cryogenia",
+        1230003,
+        8000,
+        0,
+        "Vie",
+        "Monde glacé. Civilisation insectoïde cavernicole.",
+        "planet-bck/Cryogenia.png",
+        "Réseaux de cavernes géothermiques sous calotte glaciaire.",
+        {
+            "colonies": [],
+            "lois": "Loi glaciale: 15%\nTaxe sur l'exploitation minière: 7%",
+            "marché": "Marché polaire: Glace à 30 GLD",
+            "contrats": "Contrat d'extraction de glace\nContrat de recherche géologique",
+            "inventaire": "Minéraux: 80 tonnes\nÉquipement: 4 unités"
+        }
+    ]
 }
